67cd10df7b20ee5387c4d3cf392429a5f5c37ca5
joelZheng <626279057@qq.com>
2019-04-24 20:05:56 +0800

    使用UserDefaults方法将数据保存到本地

--- TODO/TodoListViewController.swift
+++ TODO/TodoListViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
+    let defaults = UserDefaults.standard
     var itemArray = ["购买水杯","吃药","修改密码"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        if let items = defaults.array(forKey: "ToDoListArray") as? [String] {
+            itemArray = items
+        }
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -50,6 +54,7 @@ class TodoListViewController: UITableViewController {
         let action = UIAlertAction(title: "添加项目", style: .default) {(action) in
             
             self.itemArray.append(textField.text!)
+            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
             self.tableView.reloadData()
         }
         
